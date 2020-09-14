639c47399bf4042b9c7c95ccd8ea41cce428967b
Alice Nin <aliceknin@gmail.com>
2020-09-14 01:04:50 -0400

    radio buttons component

---
+++ client/src/components/RadioButtons.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const RadioButtons = (props) => {
+    return (
+        <fieldset>
+            {props.options.map((option) => 
+                <div className="form-group">
+                    <input type="radio" id={option.value} 
+                                        name={option.value} 
+                                        value={option.value} 
+                                        checked={props.checkedValue === option.value} 
+                                        onChange={props.onChange}/>
+                    <label htmlFor={option.value}>{option.displayName}</label>
+                </div>
+            )}
+        </fieldset>
+    );
+}
+
+export default RadioButtons;
--- client/src/components/SavePlaylistModal.js
+++ client/src/components/SavePlaylistModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ReactModal from 'react-modal';
 import "../styles/Modal.scss";
 import '../styles/SavePlaylistModal.scss';
+import RadioButtons from './RadioButtons';
 
 const SavePlaylistModal = (props) => {
     const [title, setTitle] = useState("");
@@ -28,6 +29,12 @@ const SavePlaylistModal = (props) => {
         props.onHide();
     }
 
+    const options = [
+        { displayName: "Public", value: "public"},
+        { displayName: "Private", value: "private"},
+        { displayName: "Collaborative", value: "collab"}
+    ]
+
     return (
         <ReactModal isOpen={props.isOpen}
                     contentLabel="Save Playlist Modal"
@@ -41,27 +48,13 @@ const SavePlaylistModal = (props) => {
                     value={title}
                     onChange={handleTitleChange}/>
                 <textarea aria-label="Description"
-                            placeholder="Description"
-                            value={description}
-                            onChange={handleDescriptionChange}/>
-                <fieldset>
-                    <div className="form-group">
-                        <input type="radio" id="public" name="public" value="public" 
-                                checked={type === "public"} onChange={handleTypeChange}/>
-                        <label htmlFor="public">Public</label>
-                    </div>
-                    <div className="form-group">
-                        <input type="radio" id="private" name="private" value="private"
-                                checked={type === "private"} onChange={handleTypeChange}/>
-                        <label htmlFor="private">Private</label>
-                    </div>
-                    <div className="form-group">
-                        <input type="radio" id="collab" name="collab" value="collab"
-                                checked={type === "collab"} onChange={handleTypeChange}/>
-                        <label htmlFor="collab">Collaborative</label>
-                    </div>
-                    <i className="fas fa-info-circle"></i>
-                </fieldset>
+                    placeholder="Description"
+                    value={description}
+                    onChange={handleDescriptionChange}/>
+                <RadioButtons options={options} 
+                    checkedValue={type}
+                    onChange={handleTypeChange}/>
+                <i className="fas fa-info-circle"></i>
                 <div className="buttons-container">
                     <button className="button-light" onClick={props.onHide}>Cancel</button>
                     <button type="submit">Save Playlist</button>
