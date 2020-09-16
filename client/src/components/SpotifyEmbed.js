20deacb223b23386eab04623d9ffe555bdec0861
Alice Nin <aliceknin@gmail.com>
2020-09-15 20:48:03 -0400

    make sure spotify embed content is always up to date on load? maybe?

--- client/src/components/SpotifyEmbed.js
+++ client/src/components/SpotifyEmbed.js
@@ -3,13 +3,20 @@ import '../styles/SpotifyEmbed.scss';
 
 const SpotifyEmbed = (props) => {
 
+    // in theory, creating a unique name on an iframe 
+    // prevents caching, so it will always have up to date
+    // content (which is very important as changing the
+    // content of a playlist is literally the point of this)
+    // see https://stackoverflow.com/questions/2648053/preventing-iframe-caching-in-browser
+
     return (
         <div className="spotify-embed-container">
             <iframe src={`https://open.spotify.com/embed/playlist/${props.playlistID}`}
-                title="Spotify Playlist" 
+                title="Spotify Playlist"
+                name={"Spotify Playlist" + Date.now().toString()}
                 className="spotify-embed"
-                frameBorder="0" 
-                allowtransparency="true" 
+                frameBorder="0"
+                allowtransparency="true"
                 allow="encrypted-media">
             </iframe>
         </div>
