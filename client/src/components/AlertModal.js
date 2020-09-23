492b01e89f3227e5aafa8ba7bdc323bbcf055ae8
Alice Nin <aliceknin@gmail.com>
2020-09-23 18:28:40 -0400

    close alerts on enter

--- client/src/components/AlertModal.js
+++ client/src/components/AlertModal.js
@@ -3,17 +3,19 @@ import ReactModal from 'react-modal';
 
 const AlertModal = (props) => {
     return (
-        <ReactModal isOpen={props.isOpen}
+        <div onKeyPress={e => e.key === "Enter" && props.onHide()}>
+            <ReactModal isOpen={props.isOpen}
                     contentLabel={props.label}
                     onRequestClose={props.onHide}
                     className="modal alert"
                     closeTimeoutMS={200}>
-            <p>{props.message}</p>
-            <div className="buttons-container">
-                <button onClick={props.onHide} 
-                        className="button-light">close</button>
+                <p>{props.message}</p>
+                <div className="buttons-container">
+                <button onClick={props.onHide}
+                    className="button-light">close</button>
             </div>
-        </ReactModal>
+            </ReactModal>
+        </div>
     );
 }
 
